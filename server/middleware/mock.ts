628040f1922c3cf73190e463008f25e01748d533
toimc <brain@toimc.com>
2023-07-05 16:36:54 +0800

    fix: 解决test.md路径无法加载的问题

--- server/middleware/mock.ts
+++ server/middleware/mock.ts
@@ -6,7 +6,9 @@ export default defineEventHandler(async (event) => {
   if (process.env.NODE_ENV === 'development') {
     // 2. 判断url -> /api Y -> redirect /mock
     const url = getRequestURL(event)
-    if (url.pathname.indexOf('/api') !== -1) {
+    // bugfix url.pathname与
+    if (url.pathname.indexOf('/api') !== -1 && url.pathname.indexOf('/api/_') === -1) {
+      console.log('in', url.pathname)
       const path = url.pathname.replace('/api', '/mock')
       return await sendProxy(event, (process.env.BASE_URL || 'http://localhost:3000') + path)
     }
